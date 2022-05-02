2869209c2e690c59be28404aaab322bc36a40828
bartek9105 <bartek9105@gmail.com>
2022-05-02 13:12:43 +0200

    add react dropzone lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import Layout from './components/Layout'
+import { useDropzone } from 'react-dropzone'
 
 function App() {
-	return <Layout>test</Layout>
+	const { draggedFiles, getRootProps, getInputProps } = useDropzone()
+
+	return (
+		<Layout>
+			<div {...getRootProps({ className: 'dropzone' })}>
+				<input {...getInputProps()} />
+				<p>Drag 'n' drop some files here, or click to select files</p>
+			</div>
+		</Layout>
+	)
 }
 
 export default App
